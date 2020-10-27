033a2485d49c55be45c8998a14f771fde3f820b5
sformichella <sformichella@gmail.com>
2020-10-27 15:32:51 -0700

    add horns filter

--- src/CreaturesList.js
+++ src/CreaturesList.js
@@ -4,40 +4,88 @@ import {capitalize} from './utils.js';
 
 export default class CreaturesList extends Component {
     state = {
-        filter: ''
+        keywordFilter: '',
+        hornsFilter: 0,
     }
 
-    handleChange = e => {
+    handleKeywordFilter = e => {
         this.setState({
-            filter: e.target.value
+            keywordFilter: e.target.value
+        })
+    }
+
+    handleHornsFilter = e => {
+        this.setState({
+            hornsFilter: Number(e.target.value)
         })
     }
 
     render() {
         const filteredCreatures = this.props.creatures.filter((creature) => {
-            if (!this.state.filter) return true;
+            const keyword = this.state.keywordFilter
+            const hornCount = this.state.hornsFilter
 
-            if (creature.keyword === this.state.filter) return true;
+            
+            if (!keyword || !hornCount) {
+                if (!keyword && !hornCount) {
+                    return true
+                }
+                if (keyword) {
+                    if (!hornCount && creature.keyword === keyword) {
+                        return true
+                    }
+                    if (creature.horns === hornCount && creature.keyword === keyword) {
+                        return true
+                    }
+                    return false
+                }
+                if (creature.horns === hornCount) {
+                    return true
+                }
+            }
 
-            return false;
+            if (creature.keyword === keyword && creature.horns === hornCount) {
+                return true
+            }
+
+            return false
         })
 
         const creatureKeywords = [...new Set(this.props.creatures.map(creature => creature.keyword))]
 
+        const creatureHorns =[...new Set(this.props.creatures.map(creature => creature.horns))]
+
         return (
             <>
             <div>
-                <select onChange={this.handleChange}>
-                    <option value="show-all">Show All</option>
+                <select onChange={this.handleKeywordFilter}>
+                    <option value="">All Creatures</option>
+
                     {
+                    // Render an option for each key in the creatureKeywords
                         creatureKeywords.map(key => {
                             return <option value={key}>{capitalize(key)}</option>
                         })
                     }
+
+                </select>
+
+                <select onChange={this.handleHornsFilter}>
+                    <option value="">All Horn Counts</option>
+
+                    {
+                        // Render an option for each horns count
+                        creatureHorns.map(horns => {
+                            return <option value={horns}>{horns}</option>
+                        })
+                    }
                 </select>
             </div>
+
             <div className="creatures-list">
+
                 {
+                // Add a CreatureItem for each creature in the hornedCreatures array
                     filteredCreatures.map(creature =>
                         <CreatureItem
                             title = {creature.title}
@@ -47,6 +95,7 @@ export default class CreaturesList extends Component {
                         />
                     )
                 }
+
             </div>
             </>
         )
